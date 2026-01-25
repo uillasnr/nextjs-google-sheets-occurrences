a16cce2796932abca8b5bc50ffe1608aeeeafa03
uillasnr <wiliasreis@hotmail.com>
2026-01-25 18:34:55 -0300

    add: type Expedição

---
+++ types/Expedicao.ts
@@ -0,0 +1,25 @@
+export type Status = "PENDENTE" | "EXPEDIDO";
+export type Filtro = "TODOS" | "PENDENTE" | "EXPEDIDO";
+
+
+export interface Expedicao {
+   id: string;
+  nota: number;
+  cliente: string;
+    dataNota: string;
+  volumes: number;
+  status: "PENDENTE" | "EXPEDIDO";
+  data?: string; // ✅ só quando expedir
+  motorista?: string;
+  cpf?: string;
+  placa?: string;
+}
+
+export interface CadastrationExpedicao {
+  id: string;
+  nota: number;
+  cliente: string;
+    dataNota: string;
+  volumes: number;
+  status: "PENDENTE";
+}
