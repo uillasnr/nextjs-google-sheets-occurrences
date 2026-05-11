956c8b3b3970238e38938231d2ccdb4a3d279e36
wilias reis <118194933+uillasnr@users.noreply.github.com>
2026-05-11 15:55:34 +0000

    Feat: add sider bar

--- app/AnaliseTransporte/page.tsx
+++ app/AnaliseTransporte/page.tsx
@@ -33,6 +33,8 @@ import {
 import AnaliseHeader from "./componete/AnaliseHeader";
 import { TabelaDetalhada } from "./componete/TabelaDetalhada";
 import MediaFreteEstadoChart from "./componete/MediaFreteEstadoChart";
+import Sidebar from "../components/Sidebar";
+import { useRouter } from "next/navigation";
 
 function parseDateBR(dateStr?: string) {
   if (!dateStr) return null;
@@ -51,9 +53,9 @@ export default function AnaliseTransporte() {
   const [estadoFiltro, setEstadoFiltro] = useState("");
   const [filialFiltro, setFilialFiltro] = useState("");
   const [statusFiltro, setStatusFiltro] = useState("");
-  const [dark, setDark] = useState(false);
   const [kpiFiltro, setKpiFiltro] = useState("");
   const [cnpjFiltro, setCnpjFiltro] = useState("");
+  const router = useRouter();
 
   // ✅ FETCH SEGURO (NUNCA QUEBRA)
   useEffect(() => {
@@ -79,11 +81,6 @@ export default function AnaliseTransporte() {
   const remetentesUnicos = Array.from(
     new Set(data.map((d) => d.cnpjRemetente).filter(Boolean))
   ).sort();
-
-  // DARK MODE
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", dark);
-  }, [dark]);
 
   // FILTROS
   let filtrado = data.filter((d) => (d.mes || "").toLowerCase() === mes);
@@ -181,249 +178,257 @@ export default function AnaliseTransporte() {
   const notasAtrasadas = filtrado.filter((d) => calcularAtraso(d) > 0).length;
 
   return (
-    <div className="min-h-screen  bg-gray-200/40 dark:bg-gray-950/95 transition-colors ">
-      <Loading isOpen={loading} text="Carregando Dados de Transporte" />
-      {/* HEADER */}
-
-      <AnaliseHeader
-        mes={mes}
-        setMes={setMes}
-        estadoFiltro={estadoFiltro}
-        setEstadoFiltro={setEstadoFiltro}
-        filialFiltro={filialFiltro}
-        setFilialFiltro={setFilialFiltro}
-        statusFiltro={statusFiltro}
-        setStatusFiltro={setStatusFiltro}
-        estados={estadosUnicos}
-        filiais={filiaisUnicas}
-        statusList={statusUnicos}
-        dark={dark}
-        setDark={setDark}
-        remetenteFiltro={cnpjFiltro}
-        setRemetenteFiltro={setCnpjFiltro}
-        remetentes={remetentesUnicos}
+    <>
+      <Sidebar
+        goToHome={() => router.push("/")}
+        goToDashboard={() => router.push("/")}
+        onSearchNF={() => true}
+        occurrences={[]}
+        sheet="SP"
       />
 
-      <div className="px-6">
-        {/* KPI PRINCIPAIS */}
-        <div className="grid grid-cols-5 gap-4 mb-6">
-          <Kpi
-            title="Total Frete"
-            value={`R$ ${metrics.totalFrete.toLocaleString("pt-BR", {
-              minimumFractionDigits: 2,
-            })}`}
-            icon={<Package className="w-5 h-5 text-brand-primary" />}
-          />
-          <Kpi
-            title="Total Entregas"
-            value={metrics.totalEntregas}
-            icon={<Truck className="w-5 h-5 text-brand-secondary" />}
-          />
-          <Kpi
-            title="Finalizadas"
-            value={metrics.finalizadas}
-            /*  highlight */
-            icon={
-              <CheckCircle2 className="w-5 h-5 text-emerald-600 dark:text-emerald-400" />
-            }
-            active={kpiFiltro === "FINALIZADO"}
-            onClick={() =>
-              setKpiFiltro(kpiFiltro === "FINALIZADO" ? "" : "FINALIZADO")
-            }
-          />
-          <Kpi
-            title="Em Andamento"
-            value={metrics.emAndamento}
-            icon={<Clock className="w-5 h-5 text-amber-500" />}
-            active={kpiFiltro === "ANDAMENTO"}
-            onClick={() =>
-              setKpiFiltro(kpiFiltro === "ANDAMENTO" ? "" : "ANDAMENTO")
-            }
-          />
-          <Kpi
-            title="Notas Atrasadas"
-            value={notasAtrasadas}
-            icon={<Clock className="w-5 h-5 text-red-500" />}
-            active={kpiFiltro === "ATRASADAS"}
-            onClick={() =>
-              setKpiFiltro(kpiFiltro === "ATRASADAS" ? "" : "ATRASADAS")
-            }
-          />
-        </div>
+      <div className="min-h-screen  bg-gray-200/40 dark:bg-gray-950/95 transition-colors ">
+        <Loading isOpen={loading} text="Carregando Dados de Transporte" />
+        {/* HEADER */}
 
-        <div className="grid grid-cols-4 gap-4 mb-6">
-          <Kpi
-            title="Tempo Médio (dias)"
-            value={`${metrics.tempoMedio} dias`}
-            icon={<Clock className="w-5 h-5 text-blue-500" />}
-          />
-          <Kpi
-            title="Eficiência"
-            value={`${metrics.eficiencia}%`}
-            type="success"
-            icon={<CheckCircle2 className="w-5 h-5 text-status-success" />}
-          />
-          <Kpi
-            title="Atraso Médio"
-            value={`${metrics.atrasoMedio} dias`}
-            type="error"
-            icon={<Clock className="w-5 h-5 text-status-error" />}
-          />
-          <Kpi
-            title="Maior Atraso"
-            value={`${metrics.maiorAtraso} dias`}
-            type="error"
-            icon={<Clock className="w-5 h-5 text-status-error" />}
-          />
-        </div>
+        <AnaliseHeader
+          mes={mes}
+          setMes={setMes}
+          estadoFiltro={estadoFiltro}
+          setEstadoFiltro={setEstadoFiltro}
+          filialFiltro={filialFiltro}
+          setFilialFiltro={setFilialFiltro}
+          statusFiltro={statusFiltro}
+          setStatusFiltro={setStatusFiltro}
+          estados={estadosUnicos}
+          filiais={filiaisUnicas}
+          statusList={statusUnicos}
+          remetenteFiltro={cnpjFiltro}
+          setRemetenteFiltro={setCnpjFiltro}
+          remetentes={remetentesUnicos}
+        />
 
-        {/* GRÁFICOS */}
-        <div className="grid grid-cols-3 gap-6 mb-6">
-          {/* BARRAS - ENTREGAS POR ESTADO */}
-          <Card title="Entregas por Estado (UF Destino)">
-            <ResponsiveContainer width="100%" height={230}>
-              <BarChart data={estadosData}>
-                <CartesianGrid
-                  strokeDasharray="3 3"
-                  stroke="oklch(0.25 0.012 240)"
-                />
-                <XAxis
-                  dataKey="uf"
-                  tickLine={false}
-                  axisLine={true}
-                  tickMargin={10}
-                  angle={0}
-                  textAnchor="end"
-                  height={60}
-                  tick={{ fill: "oklch(0.55 0.015 240)", fontSize: 12 }}
-                />
-                <YAxis
-                  width={30}
-                  tick={{ fill: "oklch(0.55 0.015 240)", fontSize: 12 }}
-                />
-                <Tooltip
-                  formatter={(value) => [`${value} entregas`, "Total"]}
-                  contentStyle={{
-                    backgroundColor: "var(--card-dark)",
-                    border: "1px solid var(--card-border)",
-                    borderRadius: "8px",
-                    color: "oklch(0.55 0.015 240)",
-                  }}
-                />
-                <Bar
-                  dataKey="value"
-                  isAnimationActive
-                  animationDuration={600}
-                  animationEasing="ease-out"
-                  fill="oklch(0.65 0.24 264)"
-                  markerStart="10"
-                />
-              </BarChart>
-            </ResponsiveContainer>
+        <div className="px-6">
+          {/* KPI PRINCIPAIS */}
+          <div className="grid grid-cols-5 gap-4 mb-6">
+            <Kpi
+              title="Custo total do frete"
+              value={`R$ ${metrics.totalFrete.toLocaleString("pt-BR", {
+                minimumFractionDigits: 2,
+              })}`}
+              icon={<Package className="w-5 h-5 text-brand-primary" />}
+            />
+            <Kpi
+              title="Total Entregas"
+              value={metrics.totalEntregas}
+              icon={<Truck className="w-5 h-5 text-brand-secondary" />}
+            />
+            <Kpi
+              title="Finalizadas"
+              value={metrics.finalizadas}
+              /*  highlight */
+              icon={
+                <CheckCircle2 className="w-5 h-5 text-emerald-600 dark:text-emerald-400" />
+              }
+              active={kpiFiltro === "FINALIZADO"}
+              onClick={() =>
+                setKpiFiltro(kpiFiltro === "FINALIZADO" ? "" : "FINALIZADO")
+              }
+            />
+            <Kpi
+              title="Em Andamento"
+              value={metrics.emAndamento}
+              icon={<Clock className="w-5 h-5 text-amber-500" />}
+              active={kpiFiltro === "ANDAMENTO"}
+              onClick={() =>
+                setKpiFiltro(kpiFiltro === "ANDAMENTO" ? "" : "ANDAMENTO")
+              }
+            />
+            <Kpi
+              title="Notas Atrasadas"
+              value={notasAtrasadas}
+              icon={<Clock className="w-5 h-5 text-red-500" />}
+              active={kpiFiltro === "ATRASADAS"}
+              onClick={() =>
+                setKpiFiltro(kpiFiltro === "ATRASADAS" ? "" : "ATRASADAS")
+              }
+            />
+          </div>
+
+          <div className="grid grid-cols-4 gap-4 mb-6">
+            <Kpi
+              title="Tempo Médio (dias)"
+              value={`${metrics.tempoMedio} dias`}
+              icon={<Clock className="w-5 h-5 text-blue-500" />}
+            />
+            <Kpi
+              title="Eficiência"
+              value={`${metrics.eficiencia}%`}
+              type="success"
+              icon={<CheckCircle2 className="w-5 h-5 text-status-success" />}
+            />
+            <Kpi
+              title="Atraso Médio"
+              value={`${metrics.atrasoMedio} dias`}
+              type="error"
+              icon={<Clock className="w-5 h-5 text-status-error" />}
+            />
+            <Kpi
+              title="Maior Atraso"
+              value={`${metrics.maiorAtraso} dias`}
+              type="error"
+              icon={<Clock className="w-5 h-5 text-status-error" />}
+            />
+          </div>
+
+          {/* GRÁFICOS */}
+          <div className="grid grid-cols-3 gap-6 mb-6">
+            {/* BARRAS - ENTREGAS POR ESTADO */}
+            <Card title="Entregas por Estado (UF Destino)">
+              <ResponsiveContainer width="100%" height={230}>
+                <BarChart data={estadosData}>
+                  <CartesianGrid
+                    strokeDasharray="3 3"
+                    stroke="oklch(0.25 0.012 240)"
+                  />
+                  <XAxis
+                    dataKey="uf"
+                    tickLine={false}
+                    axisLine={true}
+                    tickMargin={10}
+                    angle={0}
+                    textAnchor="end"
+                    height={60}
+                    tick={{ fill: "oklch(0.55 0.015 240)", fontSize: 12 }}
+                  />
+                  <YAxis
+                    width={30}
+                    tick={{ fill: "oklch(0.55 0.015 240)", fontSize: 12 }}
+                  />
+                  <Tooltip
+                    formatter={(value) => [`${value} entregas`, "Total"]}
+                    contentStyle={{
+                      backgroundColor: "var(--card-dark)",
+                      border: "1px solid var(--card-border)",
+                      borderRadius: "8px",
+                      color: "oklch(0.55 0.015 240)",
+                    }}
+                  />
+                  <Bar
+                    dataKey="value"
+                    isAnimationActive
+                    animationDuration={600}
+                    animationEasing="ease-out"
+                    fill="oklch(0.65 0.24 264)"
+                    markerStart="10"
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </Card>
+
+            {/* PIZZA - EFICIÊNCIA */}
+            <Card title="Eficiência de Entregas">
+              <ResponsiveContainer width="100%" height={220}>
+                <PieChart>
+                  <Pie
+                    data={eficienciaData}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={60}
+                    outerRadius={100}
+                    paddingAngle={4}
+                    stroke="oklch(0.65 0.24 264)" // 👈 cor da borda
+                    strokeWidth={2} // 👈 espessura
+                    label={({ name, value }) => `${name}: ${value.toFixed(1)}%`}
+                    fontSize={12}
+                  >
+                    <Cell fill="oklch(0.65 0.24 264)" />
+                    <Cell fill="#EF4444" stroke="#EF4444" />
+                  </Pie>
+                  <Tooltip
+                    contentStyle={{
+                      /*    backgroundColor: "oklch(0.55 0.015 240)", */
+                      border: "1px solid oklch(0.55 0.015 240)",
+                      borderRadius: "8px",
+                      color: "#fff",
+                    }}
+                  />
+                </PieChart>
+              </ResponsiveContainer>
+            </Card>
+
+            {/* BARRAS - STATUS DE DESPACHO */}
+            <Card title="Status de Despacho">
+              <ResponsiveContainer width="100%" height={230}>
+                <BarChart data={statusData}>
+                  <XAxis
+                    dataKey="name"
+                    tickLine={false}
+                    axisLine={false}
+                    tickMargin={10}
+                    tick={{ fill: "oklch(0.55 0.015 240)", fontSize: 12 }}
+                  />
+                  <YAxis
+                    width={30}
+                    tick={{ fill: "oklch(0.55 0.015 240)", fontSize: 12 }}
+                  />
+                  <Tooltip
+                    formatter={(value) => [`${value}`, "Total"]}
+                    contentStyle={{
+                      backgroundColor: "var(--card-dark)",
+                      border: "1px solid var(--card-border)",
+                      borderRadius: "8px",
+                      color: "oklch(0.55 0.015 240)",
+                    }}
+                  />
+                  <Bar dataKey="value" fill="oklch(0.65 0.24 264)" />
+                </BarChart>
+              </ResponsiveContainer>
+            </Card>
+          </div>
+
+          <MediaFreteEstadoChart data={filtrado} />
+
+          {/* INSIGHTS AUTOMÁTICOS */}
+          {/* INSIGHTS AUTOMÁTICOS */}
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mb-6">
+            <InsightCard
+              title="Estados com Maior Volume"
+              subtitle="Regiões com maior movimentação logística"
+              items={estadosData
+                .slice(0, 6)
+                .map((item) => `${item.uf}: ${item.value} entregas`)}
+              type="info"
+            />
+
+            <InsightCard
+              title="Estados com Maior Atraso"
+              subtitle="Regiões críticas no prazo de entrega"
+              items={metrics.topEstados
+                .slice(0, 6)
+                .map((item) => `${item.name}: ${item.value} atrasos`)}
+              type="error"
+            />
+
+            <InsightCard
+              title="Status de Despacho"
+              subtitle="Situação atual das operações"
+              items={statusData
+                .slice(0, 6)
+                .map((item) => `${item.name}: ${item.value}`)}
+              type="warning"
+            />
+          </div>
+
+          {/* TABELA DETALHADA */}
+          <Card title="Dados Detalhados">
+            <TabelaDetalhada dados={filtrado} />
           </Card>
-
-          {/* PIZZA - EFICIÊNCIA */}
-          <Card title="Eficiência de Entregas">
-            <ResponsiveContainer width="100%" height={220}>
-              <PieChart>
-                <Pie
-                  data={eficienciaData}
-                  dataKey="value"
-                  nameKey="name"
-                  cx="50%"
-                  cy="50%"
-                  innerRadius={60}
-                  outerRadius={100}
-                  paddingAngle={4}
-                  stroke="oklch(0.65 0.24 264)" // 👈 cor da borda
-                  strokeWidth={2} // 👈 espessura
-                  label={({ name, value }) => `${name}: ${value.toFixed(1)}%`}
-                  fontSize={12}
-                >
-                  <Cell fill="oklch(0.65 0.24 264)" />
-                  <Cell fill="#EF4444" stroke="#EF4444" />
-                </Pie>
-                <Tooltip
-                  contentStyle={{
-                    /*    backgroundColor: "oklch(0.55 0.015 240)", */
-                    border: "1px solid oklch(0.55 0.015 240)",
-                    borderRadius: "8px",
-                    color: "#fff",
-                  }}
-                />
-              </PieChart>
-            </ResponsiveContainer>
-          </Card>
-
-          {/* BARRAS - STATUS DE DESPACHO */}
-          <Card title="Status de Despacho">
-            <ResponsiveContainer width="100%" height={230}>
-              <BarChart data={statusData}>
-                <XAxis
-                  dataKey="name"
-                  tickLine={false}
-                  axisLine={false}
-                  tickMargin={10}
-                  tick={{ fill: "oklch(0.55 0.015 240)", fontSize: 12 }}
-                />
-                <YAxis
-                  width={30}
-                  tick={{ fill: "oklch(0.55 0.015 240)", fontSize: 12 }}
-                />
-                <Tooltip
-                  formatter={(value) => [`${value}`, "Total"]}
-                  contentStyle={{
-                    backgroundColor: "var(--card-dark)",
-                    border: "1px solid var(--card-border)",
-                    borderRadius: "8px",
-                    color: "oklch(0.55 0.015 240)",
-                  }}
-                />
-                <Bar dataKey="value" fill="oklch(0.65 0.24 264)" />
-              </BarChart>
-            </ResponsiveContainer>
-          </Card>
         </div>
-
-        <MediaFreteEstadoChart data={filtrado} />
-
-        {/* INSIGHTS AUTOMÁTICOS */}
-        {/* INSIGHTS AUTOMÁTICOS */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mb-6">
-          <InsightCard
-            title="Estados com Maior Volume"
-            subtitle="Regiões com maior movimentação logística"
-            items={estadosData
-              .slice(0, 6)
-              .map((item) => `${item.uf}: ${item.value} entregas`)}
-            type="info"
-          />
-
-          <InsightCard
-            title="Estados com Maior Atraso"
-            subtitle="Regiões críticas no prazo de entrega"
-            items={metrics.topEstados
-              .slice(0, 6)
-              .map((item) => `${item.name}: ${item.value} atrasos`)}
-            type="error"
-          />
-
-          <InsightCard
-            title="Status de Despacho"
-            subtitle="Situação atual das operações"
-            items={statusData
-              .slice(0, 6)
-              .map((item) => `${item.name}: ${item.value}`)}
-            type="warning"
-          />
-        </div>
-
-        {/* TABELA DETALHADA */}
-        <Card title="Dados Detalhados">
-          <TabelaDetalhada dados={filtrado} />
-        </Card>
       </div>
-    </div>
+    </>
   );
 }
 
